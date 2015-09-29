4048deb3620f48e0ef464ffb2fe2f5c3251c75ae
Glaine <xav-h@hotmail.fr>
2015-09-29 07:17:54 +0200

    Fix Character. Add Boss

---
+++ URPGGDE/Boss.hpp
@@ -0,0 +1,25 @@
+#ifndef BOSS_HPP_
+#define BOSS_HPP_
+
+
+#include <string>
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+class Boss : public Character{
+	private:
+		std::string lastName;
+
+
+	public:
+		std::string performAttack();
+		std::string defend();
+
+		std::string getLastName();
+		void setLastName(std::string);
+};
+
+
+#endif /* BOSS_HPP_ */
